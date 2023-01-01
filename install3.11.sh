3db5dfa6f31fae02293d283431f9f438ba6b5c14
HazemMeqdad <hazemmeqdad@gmail.com>
2023-01-01 12:28:54 +0300

    FIx some problems

--- install3.11.sh
+++ install3.11.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev 
 sudo wget https://www.python.org/ftp/python/3.11.1/Python-3.11.1.tar.xz
-sudo tar -xvf Python-3.10.0.tar.xz
-cd Python-3.10.0
+sudo tar -xvf Python-3.11.1.tar.xz
+cd Python-3.11.1
 sudo ./configure --enable-optimizations
 sudo make -j 4
 sudo make altinstall
